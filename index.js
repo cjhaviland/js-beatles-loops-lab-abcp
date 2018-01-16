0d6e1ed56f3dde3db6bc6736bc4448d2c3482643
CJ Haviland <cj.haviland@yahoo.com>
2018-01-16 17:34:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function theBeatlesPlay(musicians, instruments){
   var array = [];
   
   for (var i = 0; i < musicians.length; i++){
-    array.push(``);
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  
+  return array;
 }
